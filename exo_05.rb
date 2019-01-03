4baeb34b25cafa33503333a0722dc407176b383b
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-03 13:18:47 +0300

    adding exo_05.rb

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}" # Pour le calul de multiplication 10 fois 5 fois 11 qui va afficher 550
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" # Multiplication 10 fois 5 fois 11 fois 60 qui va afficher 33000
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" # Addition 3 plus 2 qui va afficher 5
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" # Soustraction 5 moin 7 qui va afficher -2 
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # Comparaison entre 5 et -2 cela va afficher true
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" # Comparaison entre 5 et -2 et cela va afficher true
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" # Comparaison entre 5 et -2 et cela va afficher false
