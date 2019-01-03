a1b8fa372936abc29bb5572639ad6ee2b1b90bb2
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-03 21:24:45 +0300

    adding exo_15.rb

--- vendredi/exo_15.rb
+++ vendredi/exo_15.rb
@@ -2,11 +2,13 @@
 # Cela va afficher tous les âges de l'utilisateur jusqu'en 2017.
 puts("Entrez votre année de naissance ")
 print("Mon année de naissance est: ")
+
+# déclaration des variables
 age = gets.chomp.to_i
-# condition d'age
 default = 2018
 age_two = age
 
+# Notre condition simplifiée
 if age <= 2017
   for i in age...default do
     puts ("Votre age en #{ age_two } est #{ age_two - age } ans.")
