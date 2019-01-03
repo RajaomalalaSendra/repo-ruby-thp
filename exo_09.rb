ea427f71e9493cebfefed6457120848772c1971b
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-03 13:48:31 +0300

    adding exo_09.rb

---
+++ exo_09.rb
@@ -0,0 +1,6 @@
+# Demander à l'utilisateur d'entrer son nom de famille
+puts "Veuillez entrer votre prénom s'il vous plait:"
+name = gets.chomp
+puts "Veuillez entrer votre nom de famille: "
+surname = gets.chomp
+puts "Bonjour, #{ name } #{ surname } !"
