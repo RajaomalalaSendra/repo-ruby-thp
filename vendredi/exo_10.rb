be80223c1801c990a6b0c4a34fdb50b969b0772f
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-03 20:46:26 +0300

    modification de l'exo_10.rb

--- vendredi/exo_10.rb
+++ vendredi/exo_10.rb
@@ -2,7 +2,11 @@
 # et qui va ressortir l'age que l'utilisateur a eu en 2017.
 puts("Entrez votre année de naissance ")
 print("Mon année de naissance est: ")
-age = gets.chomp
-age = age.to_i
-age_in_2017 = 2017 - age
-puts("Votre age en 2017, vous avez #{ age_in_2017 } ans.")
+age = gets.chomp.to_i
+# condition d'age
+if age <= 2017
+    age_in_2017 = 2017 - age
+    puts("Votre age en 2017, vous avez #{ age_in_2017 } ans.")
+else
+    puts("On devrait entrer un nombre inférieur à 2017")
+end
